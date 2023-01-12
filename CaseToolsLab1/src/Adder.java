cbd4605d1e9a699a7415183b5d4a407292567e67
twila08 <122504847+twila08@users.noreply.github.com>
2023-01-12 10:25:40 +0530

    Update Adder.java

--- CaseToolsLab1/src/Adder.java
+++ CaseToolsLab1/src/Adder.java
@@ -4,7 +4,7 @@ public class Adder {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Hello World,How are you?");
+		System.out.println("Hello World,I am fine.How are you?");
 	}
 
 }
